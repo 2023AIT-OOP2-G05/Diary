2de0f2c139285da84a0540a6bf8bac6b0e99ef35
SotaSugi <k22072kk@aitech.ac.jp>
2023-12-14 16:45:14 +0900

    杉山日記

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from diaries.DiarySample import DiarySample
 from diaries.TakanoDiary import TakanoDiary
 from diaries.TsuzukiDiary import TsuzukiDiary
+from diaries.SugiyamaDiary import SugiyamaDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [DiarySample(),
            TsuzukiDiary(),
-           TakanoDiary(), ]
+           TakanoDiary(),
+           SugiyamaDiary() ]
 
 
 for d in diaries:
