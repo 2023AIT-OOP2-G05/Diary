fbdfd4f7655f8fafd17631ecb771f4bc1b6d3e9b
yuto25252121 <153785307+yuto25252121@users.noreply.github.com>
2023-12-20 03:12:42 +0900

    ブランチ作り忘れたので一旦消します。（自分のところ）

--- diaries/tagawaDiary.py
+++
@@ -1,8 +0,0 @@
-from diaries.AbstractDiary import AbstractDiary
-class SakakibaraDiary(AbstractDiary):
-    def get_date(self):
-        return "2023-12-20"
-    def get_summary(self):
-        return "遅くなってごめん"
-    def get_author(self):
-        return "tagawayuto"
